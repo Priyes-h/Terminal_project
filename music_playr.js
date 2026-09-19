09b6a0688cfc1d0aba23ac83c2de9b76dcc94b37
Priyesh Choubey <your-email@example.com>
2026-09-19 19:42:05 +0530

    adder progress bar in the terminal as ## and time duration and control s list

--- music_playr.js
+++ music_playr.js
@@ -1,21 +1,143 @@
 const { spawn } = require('child_process')
-const songschoice = 'songs/BEN_10.mp3' // song path 
 let playingmusic = undefined // variable to store the spawned process
+let userchoice = 0 // variable to store the user choice
+let ispausedmusic = true // variable to store the paused state of the music
+let elapseduration = 0
+let totalduration = 0
+const fs = require("fs");
 
-process.stdin.setRawMode(true) // recive input for the enter key
+ // recive input for the enter key
 
-console.log("press enter key to play the song")
+
+const songlist = ['songs/BEN_10.mp3', 'songs/POKEMON.mp3', 'songs/SPIDER_MAN.mp3', 'songs/ULTIMATE_SPIDER_MAN.mp3'] // list of songs
+
+
+function listingSongs() {
+    console.clear()
+
+
+    // process.stdout.write('\x1b[2J')
+    
+
+    // process.stdout.write('\x1b[2;0H')
+
+
+    console.log('Available songs:')
+
+    songlist.forEach((song, index) => {
+        if ( index === userchoice) {
+            console.log(`> ${index + 1}. ${song}`)
+        } else {
+            console.log(`  ${index + 1}. ${song}`)
+        }
+    })
+
+    // PROGESS BARR BELOW 
+    if(playingmusic !== undefined && totalduration > 0){
+
+        let progress = Math.min(
+            elapseduration/ totalduration,
+            1
+        )
+
+        let barLength = 30
+
+        let filled = Math.floor(progress * barLength)
+
+        let empty = barLength - filled
+        console.log("progress BAr below ")
+     console.log(
+    `${'#'.repeat(filled)}${' '.repeat(empty)}  ${elapseduration.toFixed(1)}/${totalduration.toFixed(1)}`)
+    process.stdout.write('\x1b[1;40H')
+    console.log("Controls")
+
+    process.stdout.write('\x1b[2;40H')
+    console.log("↑ ↓  Select Song")
+
+    process.stdout.write('\x1b[3;40H')
+    console.log("Enter Play Song")
+
+    process.stdout.write('\x1b[4;40H')
+    console.log("P    Pause / Resume")
+
+    process.stdout.write('\x1b[5;40H')
+    console.log("Ctrl+C Exit")
+}}
+
+process.stdin.setRawMode(true)
+
 process.stdin.on('data', (data) => {
-
+    if(data[0] === 0x1b){
+        if ( data[1] === 0x5b){
+            if (data[2] === 0x41){ // up arrow key
+                userchoice = Math.max(userchoice - 1, 0)
+            } else if (data[2] === 0x42){ // down arrow key
+              
+                userchoice = Math.min(userchoice + 1,songlist.length - 1) 
+            }
+        }
+        listingSongs() // call the function to list the songs
+}
     if (data[0] === 0x0d){ // find the enter key
         console.log('Enter key pressed, playing song...')
+        if (playingmusic !== undefined){
+            playingmusic.kill()
+        }
+
+        elapseduration = 0
 
 
+        playingmusic = spawn("vlc", ["--intf","rc",songlist[userchoice]]) // spawn vlc with the song path
+        ispausedmusic = false // set the paused state to false
+        totalProgress(songlist[userchoice])
 
-        playingmusic = spawn("vlc", ["--intf","rc",songschoice]) // spawn vlc with the song path
+    } // pausing and resuming the song when p key is pressed
+        if(data[0] === 0x70){
+
+        playingmusic.stdin.write('pause\n')
+        ispausedmusic = !ispausedmusic // toggle the paused state
+
+        console.log("p key pressed, pausing/resuming song")
 
     }
-    console.log('Data received: ', data,'Song started playing ')
+    
+
 
 
 })
+
+function totalProgress(songPath){
+
+    const afInfoProcess = spawn(
+        "afinfo",
+        [songPath]
+    )
+
+    afInfoProcess.stdout.on("data", (data) => {
+
+        const rawOutput = data.toString()
+        console.log(rawOutput)
+        fs.appendFileSync("debug.txt", rawOutput + "\n");
+
+        totalduration = Number(rawOutput.split("estimated duration:")[1].split(" sec")[0]);
+
+    })
+
+}
+setInterval(() => {
+
+    if(
+        ispausedmusic === false &&playingmusic !== undefined &&totalduration > 0
+    ){
+
+        elapseduration += 0.05
+
+    }
+
+    listingSongs()
+
+}, 50)
+
+
+
+listingSongs() // call the function to list the songs
